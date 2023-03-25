9376ce9424937c6293edc1f275ef5257b4ac46a5
crazycardriverZ <123813665+crazycardriverZ@users.noreply.github.com>
2023-03-25 12:20:18 +0000

    Just made me own function ;)

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+function Game (speed: number) {
+	
+}
+input.onButtonPressed(Button.A, function () {
+    Runner.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    Runner.move(1)
+})
+let Runner: game.LedSprite = null
+game.startCountdown(20000)
+let Chaser = game.createSprite(0, 5)
+Runner = game.createSprite(2, 0)
+Runner.turn(Direction.Right, 90)
+Runner.set(LedSpriteProperty.Brightness, 50)
 basic.forever(function () {
 	
 })
