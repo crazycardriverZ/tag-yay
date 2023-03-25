b3b17f3c577086a02dc537e9087d4cb42586bd81
crazycardriverZ <123813665+crazycardriverZ@users.noreply.github.com>
2023-03-25 12:31:50 +0000

    Latest version: doesn't work - is bugged

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 function Game (speed: number) {
-	
+    Chaser.turn(Direction.Right, randint(0, 90))
+    Chaser.move(1)
+    Chaser.ifOnEdgeBounce()
+    if (Chaser.isTouchingEdge()) {
+        game.setScore(game.score() + 1)
+    }
+    if (Chaser.isTouching(Runner)) {
+        music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+        game.gameOver()
+    }
+    basic.pause(speed)
 }
 input.onButtonPressed(Button.A, function () {
     Runner.move(-1)
@@ -8,8 +18,9 @@ input.onButtonPressed(Button.B, function () {
     Runner.move(1)
 })
 let Runner: game.LedSprite = null
-game.startCountdown(20000)
-let Chaser = game.createSprite(0, 5)
+let Chaser: game.LedSprite = null
+game.startCountdown(30000)
+Chaser = game.createSprite(0, 5)
 Runner = game.createSprite(2, 0)
 Runner.turn(Direction.Right, 90)
 Runner.set(LedSpriteProperty.Brightness, 50)
